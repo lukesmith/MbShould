3dc4cd605794f1ff1c8b8276ee045ef823f39f90
lukesmith <stuff@lukesmith.net>
2010-09-22 23:42:09 +0100

    Added mbunit and gallio, resharper settings and initial extension

---
+++ src/MbShould.Tests/ShouldStringExtensionsTests.cs
@@ -0,0 +1,13 @@
+﻿using MbUnit.Framework;
+
+namespace MbShould.Tests
+{
+    [TestFixture]
+    public class ShouldStringExtensionsTests
+    {
+        public void should_contain_string()
+        {
+            "Contains the string".ShouldContain("the");
+        }
+    }
+}
---
+++ src/MbShould/ShouldStringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MbUnit.Framework;
+
+namespace MbShould
+{
+    public static class ShouldStringExtensions
+    {
+        public static void ShouldContain(this string actualValue, string expectedSubstring)
+        {
+            Assert.Contains(actualValue, expectedSubstring);
+        }
+    }
+}
